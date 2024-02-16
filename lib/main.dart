414935ce8d674d77f067cc88a36ea6e2c4307bf5
abhiramicdev <abhiramicpani@gmail.com>
2024-02-16 22:42:20 +0530

    fix: commments cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -60,11 +60,7 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+
     );
   }
 }
